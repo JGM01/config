c3398129691cfb8c178c2677bb49cdfb1ef99d6b
JGM01 <jacobgermanamccray@gmail.com>
2025-04-19 19:57:36 -0400

    ..

--- flake.nix
+++ flake.nix
@@ -23,11 +23,11 @@ outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }: {
 			./modules/darwin/system.nix
 			home-manager.darwinModules.home-manager 
 			{
+				users.users.jacob.home = "/Users/jacob";
 				home-manager.useGlobalPkgs = true;
 				home-manager.useUserPackages = true;
 				home-manager.users.jacob = {pkgs, ...}: {
 					imports = [./modules/common/home.nix];
-					users.users.jacob.home = "/Users/jacob";
 					home.username = "jacob";
 					home.homeDirectory = builtins.trace "Platform isDarwin: ${toString pkgs.stdenv.hostPlatform.isDarwin}" "/Users/jacob";
 				};
@@ -41,13 +41,12 @@ outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }: {
 			./modules/linux/system.nix
 			home-manager.nixosModules.home-manager
 			{
-				
+				users.users.jacob.home = "/Users/jacob";
 				home-manager.useGlobalPkgs = true;
 				home-manager.useUserPackages = true;
 				home-manager.users.jacob = {pkgs, ...}: {
 					imports = [./modules/common/home.nix];
 					home.username = "jacob";
-					users.users.jacob.home = "/home/jacob";
 					home.homeDirectory = builtins.trace "Platform isDarwin: ${toString pkgs.stdenv.hostPlatform.isDarwin}" "/home/jacob";
 				};
 			}
