5bc92b513c62a1c6fdb0c362608c77803e2c4026
JGM01 <jacobgermanamccray@gmail.com>
2025-04-23 12:04:22 -0400

    fixing nushell stuff

--- modules/common/home.nix
+++ modules/common/home.nix
@@ -13,6 +13,23 @@
 		pkgs.bun
 		pkgs.nushell
 	];
-
+	programs.nushell = {
+    		enable = true;
+    		envFile.text = ''
+      		# Source Nix profile
+      		if ("/etc/profiles/per-user/jacob/bin" | path exists) {
+        		$env.PATH = ($env.PATH | split row (char path_sep) | prepend "/etc/profiles/per-user/jacob/bin" | uniq)
+      		}
+      		if ("/Users/jacob/.nix-profile/bin" | path exists) {
+        		$env.PATH = ($env.PATH | split row (char path_sep) | prepend "/Users/jacob/.nix-profile/bin" | uniq)
+      		}
+    		'';
+    		configFile.text = ''
+      		# Your nushell config
+      		$env.config = {
+        		show_banner: false
+      		}
+    		'';
+  	};
 	home.stateVersion = "24.05";
 }
