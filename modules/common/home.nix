33536ff64e8bdf4c7d645864a0e1b6d570b9ee72
JGM01 <jacobgermanamccray@gmail.com>
2025-04-23 23:30:12 -0400

    I dont like the terminal predicting what im going to do (its wrong)

--- modules/common/home.nix
+++ modules/common/home.nix
@@ -33,8 +33,6 @@
 
 	programs.zsh = {
     		enable = true;
-    		enableCompletion = true;  
-    		autosuggestion.enable = true;  
     		syntaxHighlighting.enable = true;  
 
     		shellAliases = {
